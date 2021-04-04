271ac873eb876dc9afbad03cb6b7d75b6abf8678
Thiago_home <thiagoiklock@gmail.com>
2021-04-04 11:16:42 -0300

    remove coments and refactored them as a way to save image instead of show in line as was meant by author because he used jupyter

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # stravanalytics
+
+This repo should download, visualise and analyse my personal strava data.
+
+### Inspirational links and or credit from previously works shall be name here:
+Matt Ambrigi - https://towardsdatascience.com/using-the-strava-api-and-pandas-to-explore-your-activity-data-d94901d9bfde
+
+### Best Practices
+LOGER EMIT :https://towardsdatascience.com/stop-using-print-and-start-using-logging-a3f50bc8ab0
+
+
+### Issues
+
+matploib keeps posting warning mitgh solve with:
+https://stackoverflow.com/questions/42097053/matplotlib-cannot-find-basic-fonts
--- strava_api.py
+++ strava_api.py
@@ -1,41 +1,59 @@
+#%%
 import requests
 import urllib3
+#Using logigns instead of print
+import logging
+#Datetime will allow Python to recognize dates as dates, not strings.
+from datetime import datetime
+
+#Pandas will be the backbone of our data manipulation.
+import pandas as pd
+from pandas.io.json import json_normalize
+#Seaborn is a data visualization library.
+import seaborn as sns
+#Matplotlib is a data visualization library. 
+#Seaborn is actually built on top of Matplotlib. 
+import matplotlib.pyplot as plt
+#Numpy will help us handle some work with arrays.
+import numpy as np
+
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+logging.basicConfig(level=logging.NOTSET)
+
+
 
 auth_url = "https://www.strava.com/oauth/token"
 activites_url = "https://www.strava.com/api/v3/athlete/activities"
 
 payload = {
-    'client_id': "64115",
-    'client_secret': 'a9f44c6b3206f26b80b5b435b542203972de4472',
-    'refresh_token': 'c101042fde9c155647353200eae76387d8c47f52',
-    'grant_type': "refresh_token",
-    'f': 'json'
+    
 }
 
-print("Requesting Token...\n")
+logging.info("Requesting Token...\n")
 res = requests.post(auth_url, data=payload, verify=False)
 access_token = res.json()['access_token']
-print("Access Token = {}\n".format(access_token))
+logging.info("Access Token Successful".format(access_token))
 
 header = {'Authorization': 'Bearer ' + access_token}
 param = {'per_page': 200, 'page': 1}
 my_dataset = requests.get(activites_url, headers=header, params=param).json()
 
+logging.info('Everything is normal. Relax!')
+
 print(my_dataset[0]["name"])
 print(my_dataset[0]["map"]["summary_polyline"])
 
-
-import pandas as pd
-from pandas.io.json import json_normalize
-
 activities = json_normalize(my_dataset)
 
-#Create new dataframe with only columns I care about
+# Create new dataframe with only columns I care about
 cols = ['name', 'upload_id', 'type', 'distance', 'moving_time',   
          'average_speed', 'max_speed','total_elevation_gain',
          'start_date_local'
        ]
+
+logging.info(cols)
 
 activities = activities[cols]
 
@@ -44,27 +62,20 @@ activities['start_date_local'] = pd.to_datetime(activities['start_date_local'])
 activities['start_time'] = activities['start_date_local'].dt.time
 activities['start_date_local'] = activities['start_date_local'].dt.date
 
-activities.head(5)
+logging.info(activities.head(5))
 
-activities['type'].value_counts()
+logging.info(activities['type'].value_counts())
 
-import seaborn as sns
+runs = activities.loc[activities['type'] == 'Run']
 
-
-runs = activities.loc[activities['type'] == 'Run'] 
-runs.head(5)
-
-
-
+logging.info(runs.head(5))
 
 sns.set(style="ticks", context="talk")
 sns.scatterplot(x='distance', y = 'average_speed', data = runs).set_title("Average Speed vs Distance")
 
-
 sns.scatterplot(x='distance', y = 'max_speed', data = runs).set_title("Max Speed vs Distance")
 
-import matplotlib.pyplot as plt
-import numpy as np
+plt.savefig('save_as_a_png.png')
 
 
 # max speed over time
@@ -76,7 +87,7 @@ ax.plot_date(x, y)
 ax.set_title('Max Speed over Time')
 fig.autofmt_xdate(rotation=45)
 fig.tight_layout()
-fig.show()
+plt.savefig('save_as_a_png2.png')
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
@@ -87,3 +98,5 @@ ax1.set_title('Average Speed over Time')
 fig.autofmt_xdate(rotation=45)
 fig.tight_layout()
 fig.show()
+
+plt.savefig('save_as_a_png3.png')
